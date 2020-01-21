05d9822a69a6cf917f257024140237ebf03a142d
Andrey Sadovodov <Andrey.Sadovodov@rt.ru>
2020-01-21 12:21:22 +0300

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task("images", function () {
 
 gulp.task("webp", function () {
   return gulp.src("build/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 98}))
+    //.pipe(webp({quality: 98}))
     .pipe(gulp.dest("build/img"))
 });
 
